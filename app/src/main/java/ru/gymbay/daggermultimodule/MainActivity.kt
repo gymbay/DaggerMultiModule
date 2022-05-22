e3df8c96bf20029fe4e4f8630a1b3fbd665b962d
gymbay <aefremov430@gmail.com>
2022-05-22 13:01:57 +0300

    Update MainActivity.kt

--- app/src/main/java/ru/gymbay/daggermultimodule/MainActivity.kt
+++ app/src/main/java/ru/gymbay/daggermultimodule/MainActivity.kt
@@ -3,10 +3,4 @@ package ru.gymbay.daggermultimodule
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
